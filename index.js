4b186de4b684126d75abec72c9554818c1e35295
minhhieu99a <hieunb99@gmail.com>
2022-01-04 23:00:40 +0700

    su dung toan tu

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("Nhập chiều cao vào đây:");
+inputheight = prompt("Nhập chiều rộng vào đây:");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("Diện tích hình chữ nhật:" +area)
